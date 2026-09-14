b9e38b19a53256eccd30b091d6ee2e715257b961
juronich <87088765+juronich@users.noreply.github.com>
2026-09-14 18:23:19 +0100

    Update RailMapper.js

--- RailMapper.js
+++ RailMapper.js
@@ -99,7 +99,7 @@ Promise.all([
         });
     });
     console.log('Named railway stops:', stopsByName.size);
-});
+})
 .catch(error => console.error('Error loading railway network data:', error));
 
 const originInput = document.getElementById('origin');
