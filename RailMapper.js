2bbf0bf7f66e4ba18bff9936b4b40567dbe87cf7
juronich <87088765+juronich@users.noreply.github.com>
2026-09-14 22:20:37 +0100

    Update RailMapper.js

--- RailMapper.js
+++ RailMapper.js
@@ -189,6 +189,7 @@ fetch('data/stations.csv')
 						journeys: parseInt(journey[year], 10) || 0 
 					})) 
 				})); 
+				console.log('Years: ',yearlyJourneys);
 				const destinationStations = otherStations.map(destination => ({ 
 					station: stations.find(station => station.CRS === destination.crs), 
 					journeys: destination.journeys, 
