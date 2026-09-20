be641b95f383f9de4ff2e2632a3c4e312ae434b0
juronich <87088765+juronich@users.noreply.github.com>
2026-09-20 22:06:46 +0100

    Update RailMapper.js

--- RailMapper.js
+++ RailMapper.js
@@ -2,7 +2,7 @@ const map = L.map('map').setView([54.5, -3], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
-	className: 'osm-basemap'
+	className: 'basemap'
 }).addTo(map);
 
 const destinationLayer = L.layerGroup().addTo(map);
