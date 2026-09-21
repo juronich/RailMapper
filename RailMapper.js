21114ee3afc7cfbbdec84b147523ad5f1cf20244
juronich <87088765+juronich@users.noreply.github.com>
2026-09-21 21:43:10 +0100

    Update RailMapper.js

--- RailMapper.js
+++ RailMapper.js
@@ -423,7 +423,7 @@ function drawPassengerFlows(tree, flowData) {
         }
         //const width = 1 + (Math.sqrt(flow / maxFlow) * 12);
 		//const width = 1 + (Math.log10(flow + 1) / 6) * 14;
-		const width = 1 + (Math.pow(flow, 0.2) * 0.6);
+		const width = 1 + (Math.pow(flow, 0.2) * 0.65);
         L.polyline(coordinates, {
             color: '#3388ff',
             weight: width,
@@ -592,7 +592,7 @@ function drawDestinationRoute(tree, destinationCRS, destinationJourneys) {
 	const maxFlow = Math.max(...currentPassengerFlows.values());
 	//const width = 1 + (Math.sqrt(destinationJourneys / maxFlow) * 12);
 	//const width = 1 + (Math.log10(destinationJourneys + 1) / 6) * 14;
-	const width = 1 + (Math.pow(destinationJourneys, 0.2) * 0.6);
+	const width = 1 + (Math.pow(destinationJourneys, 0.2) * 0.65);
     const destinationStation = stations.find(
         station => station.crs === destinationCRS
     );
