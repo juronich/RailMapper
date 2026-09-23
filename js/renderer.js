84028868704c6acb1cb6e80217a20daea41b4227
juronich <87088765+juronich@users.noreply.github.com>
2026-09-23 23:04:12 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,6 +1,6 @@
 // Default and selected style configurations
 const ORIGIN_STYLE = {
-    radius: 16,
+    radius: 21,
     color: '#000',       // Teal/Green outline
     fillColor: '#2a9d8f',   // Teal/Green fill
     fillOpacity: 0.9,
