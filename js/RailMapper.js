ea038869f1fa4dc23fb6d61d88ad0ec22dc25079
juronich <87088765+juronich@users.noreply.github.com>
2026-09-25 20:21:34 +0100

    Update RailMapper.js

--- js/RailMapper.js
+++ js/RailMapper.js
@@ -68,7 +68,7 @@ function startBackgroundWorker(stations) {
 
 // Core Rendering Orchestration
 function updateVisualization() {
-	Console.time('Function: Update Visualization')
+	Console.time('Function: Update Visualization');
     clearAllMapLayers(routeLayer, destinationLayer, flowLayer, originLayer);
     if (!selectedOriginCRS) return;
 	// Draw Marker for Origin station
@@ -128,7 +128,7 @@ function updateVisualization() {
 		selectedOriginCRS,
         (destinationCRS) => handleDestinationClick(destinationCRS) // Triggers handleDestinationClick
     );
-	Console.timeEnd('Function: Update Visualization')
+	Console.timeEnd('Function: Update Visualization');
 }
 
 // Triggered when a destination station marker or search item is clicked
